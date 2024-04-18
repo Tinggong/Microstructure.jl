34f35310ac61366650830141b50d5e397fa51ad0
Ting Gong <gongting1993@gmail.com>
2024-04-18 12:31:40 -0400

    add threading

--- src/threading.jl
+++ src/threading.jl
@@ -4,27 +4,23 @@ export pre_allocate, empty_chain!, create_chain
 """
     pre_allocate(model, sampler, datasize)
 
-Allocating spaces for caching computing results based on model and sampler
-    measlen: the number of measurements in the protocol
-    datasize: the size of data; for voxel-wise parallel computing, datasize is the number of voxels within a brain mask
-        for slice-wise parallel computing, datasize is a vector of [sizex,sizey,sizez]
+Allocating spaces for caching computing results based on model, sampler and datasize
+    datasize: the size of data can be [Nmeas, Nvoxels] or [Nmeas,sizex,sizey,sizez]
 """
 function pre_allocate(model::BiophysicalModel,sampler::Sampler,datasize::Tuple{Vararg{Int64}})
     
     # temporal vectors to cache data in each mcmc; repeatedly used by threads
     measurements = [Vector{Float64}(undef, datasize[1]) for td in 1:Threads.nthreads()]
-    estimates = [model for td in 1:Threads.nthreads()]
+    estimates = [deepcopy(model) for td in 1:Threads.nthreads()]
 
     # chain space for each thread
-    chain =  create_chain(sampler,"vec")
+    chains = [create_chain(sampler,"vec") for td in 1:Threads.nthreads()]
 
-    # chain space for each thread
-    chains = [chain for td in 1:Threads.nthreads()]
+    # arrays hosting mean and std of samples
+    est = Array{Any}(undef,datasize[2:end]...,length(sampler.params))
+    est_std = Array{Any}(undef,datasize[2:end]...,length(sampler.params))
 
-    # arrays hosting fininal outputs from dataset
-    outputs = similar(chain,typeof(chain),datasize[2:end])
-
-    return measurements, estimates, chains, outputs
+    return measurements, estimates, chains, est, est_std
 end
 
 """
@@ -67,6 +63,7 @@ function create_chain(sampler::Sampler, container::String)
     else
         error("Use vec or dict")
     end
+    return chain
 end
 
 """
@@ -77,6 +74,7 @@ function empty_chain!(chain::Dict{String,Vector{Any}})
     for key in keys(chain)
         empty!(chain[key])
     end
+    return nothing
 end
 """
     create empty chain
@@ -87,8 +85,43 @@ function empty_chain(sampler::Sampler)
     # add sigma,logp and move for dignostics
     push!(chain,"sigma"=>[])
     push!(chain,"logp"=>[])
-    push!(chain,"move"=>[])    
+    push!(chain,"move"=>[])  
+    
+    return chain
 end
 
-# function threading_voxels
-# function threading_slices
+"""
+    threading(model_start, sampler, meas, protocol, noise_model)
+
+multi-threads processing of voxels or process simulated data
+"""
+function threading(model_start::BiophysicalModel, sampler::Sampler, meas::Array{Float64,2},protocol::Protocol, noise_model::Noisemodel)
+
+    datasize = size(meas)
+    pertubations = draw_samples(sampler,noise_model)
+    (measurements, estimates, chains, est, est_std) = pre_allocate(model_start,sampler,datasize)
+
+    Threads.@threads for iv in 1:datasize[2]::Int
+            
+        # for voxels in the same thread, use the allocated space repeatedly
+        td = Threads.threadid()
+        measurements[td] .= meas[:,iv]      
+        
+        # ignore voxels when normalized signals containing NaN or values larger than 1
+        sum(measurements[td]) == NaN && continue      
+        maximum(measurements[td]) > 1 && continue
+
+        # if want to use the same starting point for all voxels, add these two steps
+        update!(estimates[td],model_start,sampler.params)
+        update!(estimates[td],sampler.paralinks)
+
+        mcmc!(chains[td],estimates[td],measurements[td],protocol,sampler,pertubations)
+        
+        for ip in 1:length(sampler.params)
+            est[iv,ip] = mean(chains[td][ip][sampler.burnin:sampler.thinning:sampler.nsamples])
+            est_std[iv,ip] = std(chains[td][ip][sampler.burnin:sampler.thinning:sampler.nsamples])
+        end
+    end
+
+    return est, est_std
+end
