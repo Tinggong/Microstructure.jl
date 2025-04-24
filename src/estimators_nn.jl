8ba7595d6a6266e3c89b7afe030fdd764f2befb5
Ting Gong <gongting1993@gmail.com>
2025-04-24 14:14:07 -0400

    add saving functions and use blue style

--- src/estimators_nn.jl
+++ src/estimators_nn.jl
@@ -10,6 +10,10 @@ export NetworkArg,
     train_loop!,
     training,
     test,
+    save_mlp,
+    load_mlp,
+    nn_estimate,
+    save_nn_maps,
     losses_rmse,
     losses_corr,
     losses_rmse_kl,
@@ -48,13 +52,13 @@ Base.@kwdef struct NetworkArg
     prior_dist::Tuple{Vararg{<:Any}}
     paralinks::Union{Pair{String},Tuple{Vararg{Pair{String}}}} = ()
     noise_type::String = "Gaussian" # "Rician"    
-    sigma_range::Tuple{Float64, Float64}
+    sigma_range::Tuple{Float64,Float64}
     sigma_dist::Distribution
     nsamples::Int64
     nin::Int64
     nout::Int64
     hidden_layers::Tuple{Vararg{Int64}}
-    dropoutp::Union{<:AbstractFloat, Tuple{Vararg{<:AbstractFloat}}}
+    dropoutp::Union{<:AbstractFloat,Tuple{Vararg{<:AbstractFloat}}}
     actf::Function = relu6 # activate function for output layer
 end
 
@@ -101,14 +105,14 @@ function NetworkArg(
     model::BiophysicalModel,
     protocol::Protocol,
     params::Tuple{Vararg{String}},
-    prior_range::Tuple{Vararg{Tuple{Float64,Float64}}}, 
+    prior_range::Tuple{Vararg{Tuple{Float64,Float64}}},
     prior_dist::Tuple{Vararg{<:Any}},
     paralinks::Union{Pair{String},Tuple{Vararg{Pair{String}}}},
-    noise_type::String,   
-    sigma_range::Tuple{Float64, Float64},
+    noise_type::String,
+    sigma_range::Tuple{Float64,Float64},
     sigma_dist::Distribution,
     dropoutp=0.2,
-    actf = relu6, 
+    actf=relu6,
 )
     nin = length(protocol.bval)
     nout = 0
@@ -188,26 +192,30 @@ Return a `mlp` with `ninput`/`noutput` as the number of input/output channels, a
 'dropoutp' contains the dropout probalibities for dropout layers; it can be a single value (one dropout layer before output) or same length as the hidden layers 
 """
 function create_mlp(
-    ninput::Int, noutput::Int, hiddenlayers::Tuple{Vararg{Int}}, dropoutp::Union{<:AbstractFloat,Tuple{Vararg{<:AbstractFloat}}}, out_actf::Function=relu6,
+    ninput::Int,
+    noutput::Int,
+    hiddenlayers::Tuple{Vararg{Int}},
+    dropoutp::Union{<:AbstractFloat,Tuple{Vararg{<:AbstractFloat}}},
+    out_actf::Function=relu6,
 )
     num = (ninput, hiddenlayers...)
     layers_dense = [Dense(num[i] => num[i + 1], relu) for i in 1:(length(num) - 1)]
     if length(dropoutp) == 1
-    
-        mlp = Chain(layers_dense..., Dropout(Float32.(dropoutp)), 
-            Dense(hiddenlayers[end] => noutput, out_actf))
+        mlp = Chain(
+            layers_dense...,
+            Dropout(Float32.(dropoutp)),
+            Dense(hiddenlayers[end] => noutput, out_actf),
+        )
 
     elseif length(dropoutp)==length(hiddenlayers)
-        
         layers_dropout = [Dropout(Float32.(dropoutp[i])) for i in eachindex(dropoutp)]
         layers = Any[]
         for i in eachindex(dropoutp)
             push!(layers, layers_dense[i])
             push!(layers, layers_dropout[i])
         end
-        mlp = Chain(layers..., 
-            Dense(hiddenlayers[end] => noutput, out_actf))
-    else 
+        mlp = Chain(layers..., Dense(hiddenlayers[end] => noutput, out_actf))
+    else
         error("Numbers of dropout and hidden layer don't match")
     end
 
@@ -235,11 +243,11 @@ function generate_samples(
     model::BiophysicalModel,
     protocol::Protocol,
     params::Tuple{Vararg{String}},
-    prior_range::Tuple{Vararg{Tuple{Float64,Float64}}}, 
+    prior_range::Tuple{Vararg{Tuple{Float64,Float64}}},
     prior_dist::Tuple{Vararg{<:Any}},
     nsample::Int,
     paralinks::Union{Pair{String},Tuple{Vararg{Pair{String}}}},
-    sigma_range::Tuple{Float64, Float64},
+    sigma_range::Tuple{Float64,Float64},
     sigma_dist::Distribution,
     noise_type::String="Gaussian",
     rng_seed::Int=1,
@@ -247,41 +255,35 @@ function generate_samples(
     params_labels = []
     params_gt = Dict()
     Random.seed!(rng_seed)
-    
+
     for (p, para) in enumerate(params)
         if !hasfield(typeof(model), Symbol(para))
-            
             if isnothing(prior_dist[p])
                 vecs =
                     prior_range[p][1] .+
-                    (prior_range[p][2] - prior_range[p][1]) .*
-                    rand(Float64, nsample)
+                    (prior_range[p][2] - prior_range[p][1]) .* rand(Float64, nsample)
             else
-                vecs = 
-                    rand(truncated(prior_dist[p], 
-                    prior_range[p][1], prior_range[p][2]), 
-                    nsample)
+                vecs = rand(
+                    truncated(prior_dist[p], prior_range[p][1], prior_range[p][2]), nsample
+                )
             end
 
-            push!(
-                params_labels,
-                (vecs ./ prior_range[p][2])',
-            )
+            push!(params_labels, (vecs ./ prior_range[p][2])')
             push!(params_gt, para => vecs)
 
         elseif para == "fracs"
-            
             if model.fracs isa Vector
                 vecs = rand(prior_dist[p], nsample)
-                push!(params_labels, vecs[1:(end - 1), :])                
+                push!(params_labels, vecs[1:(end - 1), :])
                 vecs = [vecs[1:(end - 1), i] for i in 1:nsample]
             else
                 if isnothing(prior_dist[p])
                     vecs = rand(Uniform(prior_range[p][1], prior_range[p][2]), nsample)
                 else
-                    vecs = rand(truncated(prior_dist[p], 
-                    prior_range[p][1], prior_range[p][2]), 
-                    nsample)
+                    vecs = rand(
+                        truncated(prior_dist[p], prior_range[p][1], prior_range[p][2]),
+                        nsample,
+                    )
                 end
                 push!(params_labels, vecs')
             end
@@ -301,20 +303,19 @@ function generate_samples(
     end
 
     # add gaussian noise to get training inputs
-    noise_level = rand(truncated(sigma_dist, sigma_range[1], sigma_range[2]), nsample)    
+    noise_level = rand(truncated(sigma_dist, sigma_range[1], sigma_range[2]), nsample)
     if (noise_type == "Gaussian") | (noise_type == "gaussian")
         for i in 1:nsample
-            signals[:, i] .= signals[:,i] .+ rand(Normal(0, noise_level[i]), nvol)
-            signals[:,i] = signals[:,i] ./ signals[1,i]
+            signals[:, i] .= signals[:, i] .+ rand(Normal(0, noise_level[i]), nvol)
+            signals[:, i] = signals[:, i] ./ signals[1, i]
         end
     elseif (noise_type == "Rician") | (noise_type == "rician")
         for i in 1:nsample
-            signals[:,i] .=
-                sqrt.(
-                    (signals[:,i] .+ rand(Normal(0, noise_level[i]), nvol)) .^ 2.0 .+
-                    rand(Normal(0, noise_level[i]), nvol) .^ 2.0
-                )
-            signals[:,i] = signals[:,i] ./ signals[1,i]  
+            signals[:, i] .= sqrt.(
+                (signals[:, i] .+ rand(Normal(0, noise_level[i]), nvol)) .^ 2.0 .+
+                rand(Normal(0, noise_level[i]), nvol) .^ 2.0,
+            )
+            signals[:, i] = signals[:, i] ./ signals[1, i]
         end
     else
         error("Noise type not indentified")
@@ -334,27 +335,29 @@ Train and update the `mlp` and return a Dict of training logs with train loss, t
 This function works on cpu, which is sufficiently fast for most cases.
 """
 function train_loop!(
-    mlp::Chain{T}, arg::TrainingArg, inputs::Array{<:AbstractFloat,2}, labels::Array{<:AbstractFloat,2}
+    mlp::Chain{T},
+    arg::TrainingArg,
+    inputs::Array{<:AbstractFloat,2},
+    labels::Array{<:AbstractFloat,2},
 ) where {T}
-    
     opt_state = Flux.setup(Adam(arg.lr), mlp)
     tv_index = floor(Int64, size(inputs, 2) * arg.tv_split)
 
     val_set = Flux.DataLoader(
         (@views inputs[:, 1:tv_index], @views labels[:, 1:tv_index]);
         batchsize=arg.batchsize,
-    ) 
+    )
     train_set = Flux.DataLoader(
         (@views inputs[:, (tv_index + 1):end], @views labels[:, (tv_index + 1):end]);
         batchsize=arg.batchsize,
-    ) 
+    )
 
     # function to calculate validation/training data loss
     loss(mlp, x, y) = arg.lossf(mlp(x), y)
     data_loss(mlp, dataset) = mean(loss(mlp, data...) for data in dataset)
 
     train_log = Dict("train_loss" => [], "val_data_loss" => [], "train_data_loss" => [])
-    
+
     print("Training on cpu ...")
     @showprogress for epoch in 1:(arg.epoch)
         losses = 0.0
@@ -375,7 +378,7 @@ function train_loop!(
             end
             Flux.update!(opt_state, mlp, grads[1])
         end
-        
+
         #println("Epoch #" * string(epoch) * "; training loss: " * string(losses / length(train_set)))
         # Save the epoch train/val loss to log
         push!(train_log["train_loss"], losses / length(train_set))
@@ -394,30 +397,30 @@ end
 Train and return a trained `mlp` model, a Dict of training `logs` with train loss, training data loss and validation data loss for each epoch,
 and the `inputs` and `labels` (training data) the mlp was trained on. This function allows for both cpu and gpu training.
 """
-function training(arg::TrainingArg, net::NetworkArg, rng_seed::Int=1) 
-    
+function training(arg::TrainingArg, net::NetworkArg, rng_seed::Int=1)
+
     # get model and training data
     mlp, inputs, labels, gt = prepare_training(net, rng_seed)
 
-    mlp = mlp |> arg.device
+    mlp = arg.device(mlp)
     opt_state = Flux.setup(Adam(arg.lr), mlp)
     tv_index = floor(Int64, size(inputs, 2) * arg.tv_split)
 
-    val_set = Flux.DataLoader(
+    val_set = arg.device(Flux.DataLoader(
         (@views inputs[:, 1:tv_index], @views labels[:, 1:tv_index]);
         batchsize=arg.batchsize,
-    ) |> arg.device
-    train_set = Flux.DataLoader(
+    ))
+    train_set = arg.device(Flux.DataLoader(
         (@views inputs[:, (tv_index + 1):end], @views labels[:, (tv_index + 1):end]);
         batchsize=arg.batchsize,
-    ) |> arg.device
+    ))
 
     # function to calculate validation/training data loss
     loss(mlp, x, y) = arg.lossf(mlp(x), y)
     data_loss(mlp, dataset) = mean(loss(mlp, data...) for data in dataset)
 
     train_log = Dict("train_loss" => [], "val_data_loss" => [], "train_data_loss" => [])
-    
+
     print("Training on " * string(arg.device) * " ...")
     @showprogress for epoch in 1:(arg.epoch)
         losses = 0.0
@@ -438,14 +441,14 @@ function training(arg::TrainingArg, net::NetworkArg, rng_seed::Int=1)
             end
             Flux.update!(opt_state, mlp, grads[1])
         end
-        
+
         #println("Epoch #" * string(epoch) * "; training loss: " * string(losses / length(train_set)))
         # Save the epoch train/val loss to log
         push!(train_log["train_loss"], losses / length(train_set))
         push!(train_log["val_data_loss"], data_loss(mlp, val_set))
         push!(train_log["train_data_loss"], data_loss(mlp, train_set))
     end
-    mlp = mlp |> cpu
+    mlp = cpu(mlp)
     return mlp, train_log, inputs, labels, gt
 end
 
@@ -460,7 +463,6 @@ Get deterministic estimates with dropout layers off
 
 """
 function test(mlp::Chain{T}, data::Array{<:AbstractFloat,2}, ntest) where {T}
-    
     est = []
     Flux.trainmode!(mlp)
     for j in 1:ntest
@@ -475,6 +477,133 @@ function test(mlp::Chain{T}, data::Array{<:AbstractFloat,2}) where {T}
 end
 
 """
+Save a trained mlp model
+"""
+function save_mlp(
+    mlp::Chain{T}, netarg::NetworkArg, savedir::String, modelname::String
+) where {T}
+
+    # save as simpler data struct
+    mlp_state = Flux.state(mlp)
+    jldsave(joinpath(savedir, modelname * ".jld2"); mlp_state, netarg)
+end
+
+"""
+Load a mlp model
+"""
+function load_mlp(savedir::String, modelname::String)
+
+    # load mlp_state, netarg
+    loaded = JLD2.load(joinpath(savedir, modelname * ".jld2"))
+
+    # get the mlp model
+    mlp = create_mlp(
+        netarg.nin, netarg.nout, netarg.hidden_layers, netarg.dropoutp, netarg.actf
+    )
+
+    # set model state
+    Flux.loadmodel!(mlp, loaded["mlp_state"])
+
+    return mlp, loaded["netarg"]
+end
+
+"""
+   nn_estimate(
+        dmri::MRI,
+        mask::MRI,
+        mlp::Chain{T},
+        netarg::NetworkArg,
+        ntest::Int,
+        savedir::String,
+        savename::String,
+    ) 
+Apply a trained mlp model to data and save estimated parameter maps as nifti
+"""
+function nn_estimate(
+    dmri::MRI,
+    mask::MRI,
+    mlp::Chain{T},
+    netarg::NetworkArg,
+    ntest::Int,
+    savedir::String,
+    modelname::String,
+) where {T}
+    mkpath(savedir)
+
+    # get test data
+    indexing = dropdims(mask.vol; dims=4)
+    meas = Float32.(permutedims(dmri.vol[indexing .> 0, :], (2, 1)))
+
+    # inference and save maps
+    posteriors = test(mlp, meas, ntest)
+    est = mean(posteriors)
+    est_std = std(posteriors)
+
+    # save maps in standar unit
+    save_nn_maps(netarg, mask, est, est_std, savedir, modelname)
+end
+
+"""
+    save_nn_maps(
+        netarg::NetworkArg,
+        mask::MRI,
+        est::Array{<:AbstractFloat,2},
+        est_std::Array{<:AbstractFloat,2},
+        savedir::String,
+        savename::String,
+    )
+Save parameter maps
+"""
+function save_nn_maps(
+    netarg::NetworkArg,
+    mask::MRI,
+    est::Array{<:AbstractFloat,2},
+    est_std::Array{<:AbstractFloat,2},
+    savedir::String,
+    modelname::String,
+)
+
+    ### scaling with prior maximum 
+    indexing = dropdims(mask.vol; dims=4)
+
+    j = 1
+    for (i, para) in enumerate(netarg.params)
+        if !hasfield(typeof(netarg.model), Symbol(para))
+            mri = MRI(mask, 1, Float32)
+
+            mri.vol[indexing .> 0] .= est[j, :] .* netarg.prior_range[i][2]
+            mri_write(mri, joinpath(savedir, modelname * para * ".mean.nii.gz"))
+
+            mri.vol[indexing .> 0] .= est_std[j, :] .* netarg.prior_range[i][2]
+            mri_write(mri, joinpath(savedir, modelname * para * ".std.nii.gz"))
+            j = j + 1
+
+        elseif getfield(netarg.model, Symbol(para)) isa Vector
+            nf = length(getfield(model, Symbol(para)))
+            mri = MRI(mask, nf, Float32)
+
+            mri.vol[indexing .> 0, :] = est[j:(j + nf - 1), :]' .* netarg.prior_range[i][2]
+            mri_write(mri, joinpath(savedir, modelname * para * ".mean.nii.gz"))
+
+            mri.vol[indexing .> 0, :] =
+                est_std[j:(j + nf - 1), :]' .* netarg.prior_range[i][2]
+            mri_write(mri, joinpath(savedir, modelname * para * ".std.nii.gz"))
+
+            j = j + nf
+        else
+            mri = MRI(mask, 1, Float32)
+
+            mri.vol[indexing .> 0, :] .= est[j, :] .* netarg.prior_range[i][2]
+            mri_write(mri, joinpath(savedir, modelname * para * ".mean.nii.gz"))
+
+            mri.vol[indexing .> 0, :] .= est_std[j, :] .* netarg.prior_range[i][2]
+            mri_write(mri, joinpath(savedir, modelname * para * ".std.nii.gz"))
+            j = j + 1
+        end
+    end
+end
+
+"""
     RMSE loss
 """
 function losses_rmse(y, yy)
@@ -482,32 +611,35 @@ function losses_rmse(y, yy)
 end
 
 # test loss
-function losses_corr(y,yy) 
-    n=size(y,1)
-    corr = (n*sum(y.*yy,dims=1) - sum(y,dims=1).*sum(yy,dims=1))./
-        sqrt.((n*sum(y.^2,dims=1)-sum(y,dims=1).^2).*(n*sum(yy.^2,dims=1)-sum(yy,dims=1).^2))
+function losses_corr(y, yy)
+    n=size(y, 1)
+    corr =
+        (n*sum(y .* yy; dims=1) - sum(y; dims=1) .* sum(yy; dims=1)) ./ sqrt.(
+            (n*sum(y .^ 2; dims=1)-sum(y; dims=1) .^ 2) .*
+            (n*sum(yy .^ 2; dims=1)-sum(yy; dims=1) .^ 2),
+        )
     return -mean(corr)
 end
 
 function losses_rmse_kl(y, yy)
-    return 0.8*sqrt.(Flux.Losses.mse(y, yy)) + 0.2*Flux.Losses.kldivergence(y,yy)
+    return 0.8*sqrt.(Flux.Losses.mse(y, yy)) + 0.2*Flux.Losses.kldivergence(y, yy)
 end
 
 function losses_rmse_corr(y, yy)
-    return 0.8*losses_rmse(y, yy) + 0.2*losses_corr(y,yy)
+    return 0.8*losses_rmse(y, yy) + 0.2*losses_corr(y, yy)
 end
 
 """
 Identify from device to use cpu or gpu for training 
 """
 function get_backend()
-    
     device = Flux.get_device(; verbose=true)
     if typeof(device) <: Flux.FluxCPUDevice
         backend = cpu
-    elseif typeof(device) <: Union{Flux.FluxCUDADevice, Flux.FluxMetalDevice, Flux.FluxAMDGPUDevice}
+    elseif typeof(device) <:
+        Union{Flux.FluxCUDADevice,Flux.FluxMetalDevice,Flux.FluxAMDGPUDevice}
         backend = gpu
     end
-    
+
     return backend
 end
